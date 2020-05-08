f4c7cdf9a9beeda56284bb4323e4a907c7c8420a
phpunch <nutacp_ryho@hotmail.com>
2020-05-08 20:23:48 +0700

    Add POST method of card route

--- backend/routes/cardRoutes.js
+++ backend/routes/cardRoutes.js
@@ -7,4 +7,15 @@ module.exports = app => {
         const cards = await Card.find({})
         res.send(cards)
     })
+    app.post("/api/cards", async (req, res) => {
+        try {
+            const {name, status, content, category, author} = req.body
+            const card = await new Card({
+                name, status, content, category, author
+            }).save();
+            res.status(200).send(car)
+        } catch (err) {
+            res.status(400).send(err)
+        }
+    })
 }
